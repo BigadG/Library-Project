207e31c6c079c707b674698f6124020fef8de981
BIGAD GABER <BIGADGABER@icloud.com>
2023-08-30 15:17:29 -0500

    Make 'remove' button functional

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
 const myLibrary = [];
 
-function book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    return this.info = `${title} by ${author}, ${pages} pages, ${read}`
+function bookLibrary(name, author, number, read) {
+  this.name = name;
+  this.author = author;
+  this.number = number;
+  this.read = read;
+  return this.info = `${name} by ${author}, ${number} pages, ${read}`
 }
 console.log(this.info);
 
-function addBookToLibrary() {
-    const userInput = prompt("Enter something:");
-    if (userInput !== null) {
-      myLibrary.push(userInput);
-      console.log(`Added "${userInput}" to the array.`);
-      addBookToLibrary(); // Recursively call the function for more inputs
-    } else {
-      console.log("User input collection finished.");
-      console.log("User inputs:", myLibrary);
-    }
-  }
-addBookToLibrary(); // Start collecting user inputs
+
 
 
 
@@ -31,6 +20,8 @@ const books = document.getElementById("book");
 const booksect = document.getElementById("book-sect");
 const form = document.getElementById("userInputForm");
 const exit = document.getElementById("close-popup");
+const remove = document.querySelector(".remove");
+
 
 
 // Attach click event listeners to open and close popup
@@ -56,27 +47,46 @@ function handleSubmit(event) {
   const newBook = document.createElement("div");
   newBook.id = "book";
 
+  let value;
   for (let i = 0; i < inputs.length; i++) {
-      const value = form.querySelector(`#${inputs[i]}`).value;
+    let value = form.querySelector(`#${inputs[i]}`).value;
 
-      const popupItem = document.createElement("div");
-      popupItem.textContent = `${value}`;
-      newBook.appendChild(popupItem);
+    const popupItem = document.createElement("div");
+    popupItem.textContent = `${value}`;
+    newBook.appendChild(popupItem);
+    
   }
+  
+  function addBookToLibrary() {
+    if (value !== null) {
+      myLibrary.push(new bookLibrary(`${inputs[0]}`,`${inputs[1]}`,`${inputs[2]}`));
+    } else {
+      console.log("User inputs:", myLibrary);
+    }
+  }
+  addBookToLibrary(); // Start collecting user inputs
+
+
 
   const popupRead = document.createElement("div");
-  popupRead.textContent = "READ";
-  popupRead.className = "read";
+  popupRead.textContent = "NOT READ"; // Default text content
+  popupRead.className = "read"; // Apply the "read" class
+  popupRead.addEventListener("click", function () {
+    popupRead.classList.toggle("alreadyRead");
+    popupRead.textContent = popupRead.classList.contains("alreadyRead") ? "READ" : "NOT READ";
+  });
 
   const popupRemove = document.createElement("div");
   popupRemove.textContent = "REMOVE";
   popupRemove.className = "remove";
+  popupRemove.addEventListener("click", function() {
+    newBook.remove(); // Remove the parent div when "REMOVE" is clicked
+});
 
   newBook.appendChild(popupRead);
   newBook.appendChild(popupRemove);
   booksect.appendChild(newBook);
 }
-
 
 addBook.addEventListener("click", addBook);
 enterBttn.addEventListener("click", enterBttn);
@@ -85,22 +95,39 @@ exit.addEventListener("click", closePopup);
 
 
 
-  const toggleButton = document.getElementById("read");
-  let isToggled = toggleButton.classList.contains("alreadyRead");
+const toggleButton = document.querySelector(".read");
+let isToggled = toggleButton.classList.contains("alreadyRead");
 
-  function updateButtonContent() {
-      if (isToggled) {
-          toggleButton.textContent = "READ";
-      } else {
-          toggleButton.textContent = "NOT READ";
-      }
+function updateButtonContent() {
+  if (isToggled) {
+    toggleButton.textContent = "READ";
+  } else {
+    toggleButton.textContent = "NOT READ";
   }
+}
 
-  function handleClick() {
-      toggleButton.classList.toggle("alreadyRead", !isToggled);
-      isToggled = !isToggled;
-      updateButtonContent();
-  }
+function handleClick() {
+  toggleButton.classList.toggle("alreadyRead", !isToggled);
+  isToggled = !isToggled;
+  updateButtonContent();
+}
 
-  toggleButton.addEventListener("click", handleClick);
-  updateButtonContent(); // Set initial button text
+toggleButton.addEventListener("click", handleClick);
+updateButtonContent(); // Set initial button text
+
+
+
+const bookEntries = document.querySelectorAll("#book");
+
+// Loop through each book entry and attach a "REMOVE" button event listener
+bookEntries.forEach(bookEntry => {
+    const removeButton = bookEntry.querySelector(".remove");
+    
+    removeButton.addEventListener("click", function() {
+        bookEntry.remove(); // Remove the parent div (book entry) when "REMOVE" is clicked
+    });
+});
+
+
+//Next: integrate original library prototype section to rest of code,
+//Modify input boxes on popup so they're better displayed
